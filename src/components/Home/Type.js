b7b94855f89214d4a541f9ed074b4de7f66c0900
Shashank Singh <98162874+singhshashank08@users.noreply.github.com>
2024-10-13 16:45:41 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -10,7 +10,6 @@ function Type() {
           "Software Developer",
           "UI/UX Designer",
           "MERN Stack Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
